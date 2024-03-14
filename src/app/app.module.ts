ceb87abd2599637a0ec7e3b8c1362194886e8403
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2024-03-14 02:44:48 +0200

    remove imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
