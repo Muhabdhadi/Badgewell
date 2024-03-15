675b13984c6502655445ec4051a4f6db2ed185a6
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2024-03-15 02:51:23 +0200

    add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {authGuard} from "./core/auth.guard";
 
 const routes: Routes = [
     { path: '', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-    { path: 'contacts', loadChildren: () => import('./contacts/contacts.module').then(m => m.ContactsModule) }
+
+    {
+        path: 'contacts',
+        loadChildren: () => import('./contacts/contacts.module').then(m => m.ContactsModule),
+        canActivate: [authGuard]
+    }
 ];
 
 @NgModule({
---
+++ src/app/core/auth.guard.ts
@@ -0,0 +1,18 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+import {AuthHelperService} from "../auth/services/auth-helper.service";
+
+export const authGuard: CanActivateFn = (route, state) => {
+    const authHelperService = inject(AuthHelperService);
+    const router = inject(Router);
+
+    const tokens = authHelperService.getToken();
+
+    if (tokens?.accessToken) {
+        return true;
+    } else {
+        router.navigate(['login'])
+        return false;
+    }
+
+};
